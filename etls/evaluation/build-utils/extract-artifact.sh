0f408661ab93e5ac5b60228573ee1ed1007794ab
Pramod Upamanyu <pramod.upamanyu@gmail.com>
2020-08-14 12:59:45 -0700

    Added build-utils script, cloudbuild yaml updated

---
+++ etls/evaluation/build-utils/extract-artifact.sh
@@ -0,0 +1,1 @@
+# Extract Artifact deb from artifact registry, and push DAG directory to composer GCS DAG DIR
---
+++ etls/evaluation/build-utils/publish-artifact.sh
@@ -0,0 +1,2 @@
+# push artifact.sh
+gcloud alpha artifacts packages import artifact-repo --location=us-central1  --gcs-source=gs://pramodrao-dataengg-avroload/code_1.39.2-1571154070_amd64.deb
