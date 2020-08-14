532236818d61fa183f4639b9f1ad431d6e72ede2
Pramod Upamanyu <pramod.upamanyu@gmail.com>
2020-08-14 16:52:13 -0700

    More commits

--- etls/evaluation/build-utils/publish-artifact.sh
+++ etls/evaluation/build-utils/publish-artifact.sh
@@ -1,3 +1,3 @@
 # push artifact.sh
-ls -lR /workspace/cicd/bazel-bin
+find cicd/bazel-bin/etls/evaluation -type f -ls
 echo gcloud alpha artifacts packages import artifact-repo --location=us-central1 --gcs-source=gs://pramodrao-dataengg-avroload/code_1.39.2-1571154070_amd64.deb
