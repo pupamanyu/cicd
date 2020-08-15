052be9ba88730263d776596e5d3d82179d9399e1
Pramod Upamanyu <pramod.upamanyu@gmail.com>
2020-08-14 18:20:29 -0700

    More commits

--- etls/evaluation/build-utils/publish-artifact.sh
+++ etls/evaluation/build-utils/publish-artifact.sh
@@ -1,20 +1,28 @@
 # push artifact.sh
 find /workspace/cicd/bazel-bin/etls/evaluation -type f -ls
 
-install_gcloud_alpha() {
-    # Needed until Artifact Registry supports deb package
-    gcloud components install alpha
-}
+ARTIFACTBUCKET=gs://pramodrao-dataengg-avroload
+ARTIFACTDIR=/workspace/cicd/bazel-bin/etls/evaluation
+ARTIFACT=game-event_1.0.0_amd64.deb
+ARTIFACTREPO=artifact-repo
+REGION=us-central1
 
 copy_artifact_to_gcs() {
     # Needed since Artifact Registry takes input artifacts from only GCS location at the moment
-    gsutil -m cp /workspace/cicd/bazel-bin/etls/evaluation/game-event_1.0.0_amd64.deb gs://pramodrao-dataengg-avroload/
+    gsutil -m cp ${ARTIFACTDIR}/${ARTIFACT} ${ARTIFACTBUCKET} \
+    && return 0
 }
 
 upload_artifact() {
     # Upload deb artifact to artifact registry
-    gcloud alpha artifacts packages import artifact-repo --location=us-central1 --gcs-source=gs://pramodrao-dataengg-avroload/code_1.39.2-1571154070_amd64.deb
+    gcloud alpha artifacts packages import ${ARTIFACTREPO} \
+    --location=${REGION} \
+    --gcs-source=${ARTIFACTBUCKET}/${ARTIFACT} \
+    && return 0
 }
 
-copy_artifact_to_gcs
-upload_artifact
+publish_artifact() {
+    copy_artifact_to_gcs && upload_artifact
+}
+
+publish_artifact
