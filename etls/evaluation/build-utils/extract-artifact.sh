2884f67fd55e22268cb76397791fa6b9023e0d50
Pramod Upamanyu <pramod.upamanyu@gmail.com>
2020-08-14 20:27:23 -0700

    More commits

--- etls/evaluation/build-utils/extract-artifact.sh
+++ etls/evaluation/build-utils/extract-artifact.sh
@@ -1,3 +1,17 @@
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
 # Extract Artifact deb from artifact registry, and push DAG directory to composer GCS DAG DIR
 
 REGION=us-central1
@@ -10,7 +24,7 @@ AIRFLOWDAGDIR=gs://pramodrao-dataengg-avroload/dag
 
 configure_apt() {
     pwd
-    echo "deb [ trusted=yes ] https://${REGION}-apt.pkg.dev/projects/${PROJECT} ${REPOSITORY} main" >> /etc/apt/sources.list
+    echo "deb [ trusted=yes ] https://${REGION}-apt.pkg.dev/projects/${PROJECT} ${REPOSITORY} main" >>/etc/apt/sources.list
     apt-get update
 }
 
@@ -22,15 +36,17 @@ extract_package() {
     # extract the dag from downloaded .deb
     echo "TODO: Extract dag from .deb"
     ar xv ${ARTIFACT}_${ARTIFACTVERSION}_${ARTIFACTARCH}.deb
-    tar tjf data.tar.bz2
+    tar xj data.tar.bz2
 }
 
-push_dag_to_airflow() {
-    # use gsutil -m cp -r .. to push the exploded dag directory to airflow dag bucket on GCS
-    echo "TODO: Push dag to Airflow DAG bucket" 
+deploy_dag_to_airflow() {
+    # Deploy DAG to airflow 
+    configure_apt \
+    && download_package \
+    && extract_package \
+    && gsutil -m cp -r game_event ${AIRFLOWDAGDIR}
 }
 
-configure_apt
-download_package
-extract_package
-push_dag_to_airflow
+# Main
+deploy_dag_to_airflow
+
