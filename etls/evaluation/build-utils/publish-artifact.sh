bff5edf76715d0b02fbf64ebe7500067e49a91eb
Pramod Upamanyu <pramod.upamanyu@gmail.com>
2020-08-14 17:44:59 -0700

    More commits

--- etls/evaluation/build-utils/publish-artifact.sh
+++ etls/evaluation/build-utils/publish-artifact.sh
@@ -1,3 +1,20 @@
 # push artifact.sh
 find /workspace/cicd/bazel-bin/etls/evaluation -type f -ls
-echo gcloud alpha artifacts packages import artifact-repo --location=us-central1 --gcs-source=gs://pramodrao-dataengg-avroload/code_1.39.2-1571154070_amd64.deb
+
+install_gcloud_alpha() {
+    # Needed until Artifact Registry supports deb package
+    gcloud components install alpha
+}
+
+copy_artifact_to_gcs() {
+    # Needed since Artifact Registry takes input artifacts from only GCS location at the moment
+    gsutil -m cp /workspace/cicd/bazel-bin/etls/evaluation/game-event_1.0.0_amd64.deb gs://pramodrao-dataengg-avroload/
+}
+
+upload_artifact() {
+    # Upload deb artifact to artifact registry
+    echo gcloud alpha artifacts packages import artifact-repo --location=us-central1 --gcs-source=gs://pramodrao-dataengg-avroload/code_1.39.2-1571154070_amd64.deb
+}
+
+install_gcloud_alpha
+copy_artifact_to_gcs
