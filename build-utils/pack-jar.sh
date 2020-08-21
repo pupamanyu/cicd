eae99b8a346bec358e3fcec3db877a3d8f493aa3
tutikashilpa <tutikashilpa@gmail.com>
2020-08-21 07:19:06 -0700

    Update pack-jar.sh

--- build-utils/pack-jar.sh
+++ build-utils/pack-jar.sh
@@ -46,6 +46,7 @@ pack_jar() {
   echo "manifest .. ${MANIFESTTXT}"
   gen_manifest > ${MANIFESTTXT} 2> /dev/null \
   && jar cmf ${MANIFESTTXT} ${ARTIFACTJARNAME} -C ${ARTIFACTBASEDIR} ${ARTIFACTDIR} \
+  && cp ${ARTIFACTJARNAME} /tmp/bzl \
   && echo "Artifact packed into a JAR successfully"
 }
 pack_jar
