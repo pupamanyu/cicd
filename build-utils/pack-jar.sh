afac81b998f902c9946b8e91b2b38aadc25f6b08
tutikashilpa <tutikashilpa@gmail.com>
2020-08-20 22:50:05 -0700

    test

--- build-utils/pack-jar.sh
+++ build-utils/pack-jar.sh
@@ -1,22 +1,39 @@
 #!/usr/bin/env bash
 #
 #Todo : remove hardcoded path
-#cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
-cd /workspace/cicd/
+cd ${WORKSPACE}
+# Manifest related variables
+NAME="etls/evaluation/game-1"
+SPECTITLE="Game Event End to End ETL"
+SPECVERSION="$(git rev-parse --abbrev-ref HEAD)"
+SPECVENDOR="Example Company, Inc."
+IMPLTITLE="etls.evaluation"
+IMPLVERSION="$(git rev-parse HEAD)"
+IMPLVENDOR="Example Company, Inc."
 
-echo "Workspace directory: ${PWD}"
+# Artifact related variables
+WORKSPACEDIR="$(git rev-parse --show-toplevel)"
+ARTIFACTBASEDIR="${WORKSPACEDIR}/${NAME}"
+BAZELBINDIR="${WORKSPACEDIR}/bazel-bin"
+BAZELBINARTIFACTSDIR="$(bazel info bazel-bin ${BAZELBINDIR}/etls/evaluation"
+gen_manifest() {
+  echo -e "Name: ${NAME} \nSpecification-Title: ${SPECTITLE}\nSpecification-Version: ${SPECVERSION}\nSpecification-Vendor: ${SPECVENDOR}\nImplementation-Title: ${IMPLTITLE}\nImplementation-Version: ${IMPLVERSION}\nImplementation-Vendor: ${IMPLVENDOR}\n"
+}
+pack_jar() {
+  local DEBUG=1
+  if [ ${DEBUG} -eq 1 ]; then
+    echo "WORKSPACEDIR ... ${WORKSPACEDIR}"
+    echo "MANIFESTTXT from outside.. ${MANIFESTTXT}"
+    echo "ARTIFACTJARNAME .. ${ARTIFACTJARNAME}"
+    echo "ARTIFACTBASEDIR .. ${ARTIFACTBASEDIR}"
+    echo "ARTIFACTDIR .. ${ARTIFACTDIR}"
+  fi
 
-echo "commit sha is $(git rev-parse HEAD)"
-echo "bazel  info is $(bazel info bazel-bin)"
-
-echo "before path WORKSPACE ... ${WORKSPACE}"
-echo "path is ... $(pwd)"
-echo "before path is ... ${BASH_SOURCE[0]}"
-DIRECTORY=`dirname $0`
-echo "Direcory is .. $DIRECTORY"
-echo "path is ... $(pwd)"
-
-for file in "${PWD}/"*
-do
-  echo "file is ${file}"
-done
+  local TEMPDIR="$(mktemp --directory)"
+  local MANIFESTTXT="${TEMPDIR}/manifest.txt"
+  local ARTIFACTJARNAME="${TEMPDIR}/game-event_${IMPLVERSION}_${SPECVERSION}.jar"
+  gen_manifest > ${MANIFESTTXT} 2> /dev/null \
+  && jar cmf ${MANIFESTTXT} ${ARTIFACTJARNAME} -C ${ARTIFACTBASEDIR} * \
+  && echo "Artifact packed into a JAR successfully"
+}
+pack_jar
