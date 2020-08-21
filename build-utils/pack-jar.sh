e3933f96999a995868baa9916ad771a2446e1840
tutikashilpa <tutikashilpa@gmail.com>
2020-08-20 17:07:41 -0700

    test

--- build-utils/pack-jar.sh
+++ build-utils/pack-jar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 #Todo : remove hardcoded path
-cd ${WORKSPACE}
+#cd ${WORKSPACE}
 # Manifest related variables
 NAME="etls/evaluation/game-1"
 SPECTITLE="Game Event End to End ETL"
@@ -15,7 +15,7 @@ IMPLVENDOR="Example Company, Inc."
 WORKSPACEDIR="$(git rev-parse --show-toplevel)"
 ARTIFACTBASEDIR="${WORKSPACEDIR}/${NAME}"
 BAZELBINDIR="${WORKSPACEDIR}/bazel-bin"
-BAZELBINARTIFACTSDIR="$(bazel info bazel-bin ${BAZELBINDIR}/etls/evaluation"
+#BAZELBINARTIFACTSDIR="$(bazel info bazel-bin ${BAZELBINDIR}/etls/evaluation"
 
 
 gen_manifest() {
@@ -23,22 +23,34 @@ gen_manifest() {
 }
 
 pack_jar() {
-  local DEBUG=1
-  if [ ${DEBUG} -eq 1 ]; then 
-    echo "WORKSPACEDIR ... ${WORKSPACEDIR}"
-    echo "MANIFESTTXT from outside.. ${MANIFESTTXT}"
-    echo "ARTIFACTJARNAME .. ${ARTIFACTJARNAME}"
-    echo "ARTIFACTBASEDIR .. ${ARTIFACTBASEDIR}"
-    echo "ARTIFACTDIR .. ${ARTIFACTDIR}"
-  fi
-  
-  local TEMPDIR="$(mktemp --directory)"
-  local MANIFESTTXT="${TEMPDIR}/manifest.txt"
+  local MANIFESTTXT=./manifest.txt
   local ARTIFACTJARNAME="${TEMPDIR}/game-event_${IMPLVERSION}_${SPECVERSION}.jar"
-
+#  cd ${BAZELBINDIR}
   gen_manifest > ${MANIFESTTXT} 2> /dev/null \
-  && jar cmf ${MANIFESTTXT} ${ARTIFACTJARNAME} -C ${ARTIFACTBASEDIR} * \
+  && jar cmf ${MANIFESTTXT} ${ARTIFACTJARNAME} -C * \
   && echo "Artifact packed into a JAR successfully"
 }
+
+#
+#pack_jar() {
+#  local DEBUG=1
+#  if [ ${DEBUG} -eq 1 ]; then
+#    echo "WORKSPACEDIR ... ${WORKSPACEDIR}"
+#    echo "MANIFESTTXT from outside.. ${MANIFESTTXT}"
+#    echo "ARTIFACTJARNAME .. ${ARTIFACTJARNAME}"
+#    echo "ARTIFACTBASEDIR .. ${ARTIFACTBASEDIR}"
+#    echo "ARTIFACTDIR .. ${ARTIFACTDIR}"
+#  fi
+#
+#  local TEMPDIR="$(mktemp --directory)"
+#  local MANIFESTTXT="${TEMPDIR}/manifest.txt"
+#  local ARTIFACTJARNAME="${TEMPDIR}/game-event_${IMPLVERSION}_${SPECVERSION}.jar"
+#
+#  gen_manifest > ${MANIFESTTXT} 2> /dev/null \
+#  && jar cmf ${MANIFESTTXT} ${ARTIFACTJARNAME} -C ${ARTIFACTBASEDIR} ${} \
+#  && echo "Artifact packed into a JAR successfully"
+#}
+
+
 
 pack_jar
