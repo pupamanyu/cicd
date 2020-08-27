6ae6d68333e8640fe7f1bca697b0d7e20a029fc9
tutikashilpa <tutikashilpa@gmail.com>
2020-08-27 08:59:33 -0700

    test

--- build-utils/publish-artifact.sh
+++ build-utils/publish-artifact.sh
@@ -11,8 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# push artifact.sh
-#cd $(git rev-parse --show-toplevel)
+# publish-artifact.sh
+
 EXECPATH=$(pwd)
 BAZEL_WORKSPACE=${1}
 cd ${BAZEL_WORKSPACE}
